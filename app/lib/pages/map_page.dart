1b45fd5acb99e545f3e61585511f4e11e9676860
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 12:00:34 -0800

    macos

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key, required this.title});
@@ -12,8 +13,6 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Scaffold();
   }
 }
