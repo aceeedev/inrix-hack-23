9b4539eb1c86c56db9d7cc24dd1ac02e3da23ae8
aceeedev <cyberandrew03@gmail.com>
2023-11-11 11:48:12 -0800

    Added DB and initial ticket card

---
+++ app/lib/models/event.dart
@@ -0,0 +1,13 @@
+class Event {
+  final String name;
+  final String address;
+  final DateTime date;
+  final String url;
+
+  Event({
+    required this.name,
+    required this.address,
+    required this.date,
+    required this.url,
+  });
+}
