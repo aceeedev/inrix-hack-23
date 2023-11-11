316176c9796cdd78c03672022401b3ff29bdc964
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 12:43:05 -0800

    map page

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -11,8 +11,43 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('Sydney'), backgroundColor: Colors.green[700]),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          markers: {
+            const Marker(
+              markerId: MarkerId('Sydney'),
+              position: LatLng(-33.86, 151.20),
+              infoWindow: InfoWindow(
+                title: "Sydney",
+                snippet: "Capital of New South Wales",
+              ),
+            )
+          },
+        ),
+      ),
+    );
   }
 }
