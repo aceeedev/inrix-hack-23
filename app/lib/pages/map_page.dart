c7bd9aa51670b9dd3e2301c3e6ae2eaa8e474706
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 11:54:51 -0800

    map

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -10,22 +10,10 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      GoogleMap(
-        initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-        ),
-        markers: {
-            const Marker(
-              markerId: MarkerId('Sydney'),
-              position: LatLng(-33.86, 151.20),
-            )
-        },
-      )
+      
     );
   }
 }
