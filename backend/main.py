687df8e1f6fd872526c6e8074632ff3aefdb6898
Lucas Jablon <lucas.jablon@gmail.com>
2023-11-11 18:01:51 -0800

    Bearer Token Implementation

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,10 @@ from services.class3 import TicketMasterServices
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Home of Swifty Service"
+
 @app.route("/tickets")
 def get_tickets():
     return TicketMasterServices.run_get_tickets()
