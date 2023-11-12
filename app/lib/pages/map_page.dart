0e3ec9734fdce3c3bd784fdcf9f251e5d471962b
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 20:45:52 -0800

    map

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -2,6 +2,7 @@ import 'package:app/pages/saved_tickets_page.dart';
 import 'package:app/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/retry.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key, required this.title});
@@ -33,42 +34,42 @@ class _MapPageState extends State<MapPage> {
     ], // replace with temp points
     endCap: Cap.roundCap,
   );
-  Polyline transitline = const Polyline(polylineId: PolylineId("transitline"));
+  Polyline transitline =
+      const Polyline(polylineId: PolylineId("awqeF|h_jVv@dATXCDMN"));
   Polyline returnline = const Polyline(polylineId: PolylineId("returnline"));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Routing"),
-        backgroundColor: Styles().mainColor,
-        titleTextStyle:
-            const TextStyle(color: Colors.white, fontFamily: 'JetBrains Mono'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          onPressed: () => Navigator.of(context).pop(
-            // the top
-            MaterialPageRoute(
-              builder: (context) => const SavedTicketsPage(),
+        body: Stack(
+      children: [
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          // cloudMapId: '40bae229feee19e5', // javascript vector
+          // cloudMapId: '5b05b9f927bfcf34',  // javascript raster
+          cloudMapId: '701a336f83a1aaa6', // static raster
+          // cloudMapId: '74194f22342551fa',  // android
+          polylines: <Polyline>{driveline, transitline, returnline},
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          myLocationButtonEnabled: false,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 32, left: 16),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: TextButton(
+              onPressed: () => Navigator.of(context).pop(MaterialPageRoute(
+                builder: (context) => const SavedTicketsPage(),
+              )),
+              style: const ButtonStyle(splashFactory: NoSplash.splashFactory),
+              child: const Icon(Icons.arrow_back_ios_new),
             ),
           ),
         ),
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        // cloudMapId: '40bae229feee19e5', // javascript vector
-        // cloudMapId: '5b05b9f927bfcf34',  // javascript raster
-        cloudMapId: '701a336f83a1aaa6', // static raster
-        // cloudMapId: '74194f22342551fa',  // android
-        polylines: <Polyline>{driveline},
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-        myLocationButtonEnabled: false,
-      ),
-    );
+      ],
+    ));
   }
 }
