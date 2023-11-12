4c70baad02bfd4cbf2b747edab011688dea21324
Riley Wong <rnwong@scu.edu>
2023-11-11 22:32:24 -0800

    Update maps for polyline (in progress)

--- Backend/services/google_maps_service.py
+++ Backend/services/google_maps_service.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import json
+import polyline
 
 from credentials_service import CredentialsService
 
@@ -19,6 +20,40 @@ class GoogleMapsService:
         response = response.json()
         
         # filter 
+
+        start_info = response["routes"][0]["legs"][0]["start_address"]
+        end_info = response["routes"][0]["legs"][0]["end_address"]
+        try:
+            fare = response["routes"]["fare"]["text"]
+        except:
+            fare = None
+
+
+        points = []
+
+        for step in response["routes"][0]["legs"][0]["steps"]:
+
+            poly = polyline.decode(step["polyline"]["points"])
+            points.extend(poly)
+
+            # try:
+            #     sub_points = []
+            #     for sub_step in step["steps"]:
+            #         lat = sub_step["start_location"]["lat"]
+            #         long = sub_step["start_location"]["lng"]
+            #         sub_points.append([lat, long])
+            #     points.append(sub_points)
+
+            # except:
+            #     lat = step["start_location"]["lat"]
+            #     long = step["start_location"]["lng"]
+            #     points.append([[lat, long]])
+
+        pprint(points)
+
+        return {"start": start_info, "end": end_info, "fare": fare, "points": points}
+
+        print(points)
 
         with open("transit.json", "w") as outfile:
             json.dump(response, outfile)
