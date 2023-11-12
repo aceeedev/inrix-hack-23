6aa768cda7e68bd1134f3d93888766da42687512
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 22:47:03 -0800

    stuff

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:app/pages/begin_page.dart';
+import 'package:app/backend/flask_interface.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key, required this.title});
@@ -26,20 +27,7 @@ class _MapPageState extends State<MapPage> {
   Polyline driveline = const Polyline(
     polylineId: PolylineId("driveline"),
     color: Colors.blue,
-    points: <LatLng>[
-      LatLng(37.7792079, -122.3900709),
-      LatLng(37.77806169999999, -122.3915693),
-      LatLng(37.78166050000001, -122.3961541),
-      LatLng(37.7993974, -122.4087114),
-      LatLng(37.79948880000001, -122.4087865),
-      LatLng(37.7997396, -122.4066119),
-      LatLng(37.80161289999999, -122.4069633),
-      LatLng(37.80185669999999, -122.4056576),
-      LatLng(37.8020986, -122.4059576),
-      LatLng(37.8021366, -122.4058373),
-      LatLng(37.8022515, -122.405858),
-      LatLng(37.8022916, -122.4056988)
-    ], // replace with temp points
+    points: [LatLng(37.7775, -122.416389)], // replace with temp points
     endCap: Cap.roundCap,
   );
   // Polyline transitline =
@@ -65,7 +53,6 @@ class _MapPageState extends State<MapPage> {
               style: Styles().largeTextStyle,
             ),
           ),
-          ..._getRouteDirections(),
         ],
       ),
       borderRadius: const BorderRadius.only(
@@ -74,9 +61,9 @@ class _MapPageState extends State<MapPage> {
         children: [
           GoogleMap(
             onMapCreated: _onMapCreated,
-            // cloudMapId: '40bae229feee19e5', // javascript vector
+            cloudMapId: '40bae229feee19e5', // javascript vector
             // cloudMapId: '5b05b9f927bfcf34',  // javascript raster
-            cloudMapId: '701a336f83a1aaa6', // static raster
+            // cloudMapId: '701a336f83a1aaa6', // static raster
             // cloudMapId: '74194f22342551fa',  // android
             polylines: <Polyline>{driveline},
             markers: parkSpots,
@@ -100,9 +87,5 @@ class _MapPageState extends State<MapPage> {
         ],
       ),
     ));
-  }
-
-  List<Widget> _getRouteDirections() {
-    return [];
   }
 }
