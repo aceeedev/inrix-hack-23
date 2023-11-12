c35e1db39674566a71630b84fc4e16822408adf2
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-12 06:42:38 -0800

    Revert "Updated begin page background"
    This reverts commit 5d613ab1edbc052505438004dc5ef005f9756836.

--- app/lib/pages/begin_page.dart
+++ app/lib/pages/begin_page.dart
@@ -2,6 +2,7 @@ import 'package:app/pages/find_page.dart';
 import 'package:flutter/material.dart';
 import 'package:app/styles.dart';
 import 'package:app/widgets/gradient_button_widget.dart';
+import 'package:app/widgets/roads_gradient_widget.dart';
 
 class BeginPage extends StatelessWidget {
   const BeginPage({super.key});
@@ -9,44 +10,39 @@ class BeginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage("assets/images/Background.png"),
-          fit: BoxFit.fill,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Park, Bus, & Jam',
-              style: Styles().largeTextStyle,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const RoadsGradient(),
+          Text(
+            'Park, Bus, & Jam',
+            style: Styles().largeTextStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 30.0, 12.0, 12.0),
+            child: Text(
+              'Seamless parking, stress-free transit, and immersion in the concert experience.',
+              textAlign: TextAlign.center,
+              style: Styles().defaultTextStyle.copyWith(color: Colors.black),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12.0, 16.0, 12.0, 25.0),
+          ),
+          const Spacer(),
+          Padding(
+            padding: Styles().bottomButtonEdgeInset,
+            child: GradientButton(
+              onPressed: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => const FindPage(),
+                ),
+              ),
               child: Text(
-                'Seamless parking, stress-free transit, and immersion in the concert experience.',
-                textAlign: TextAlign.center,
-                style: Styles().defaultTextStyle.copyWith(color: Colors.black),
+                'Begin',
+                style: Styles().mediumTextStyle.copyWith(color: Colors.white),
               ),
             ),
-            Padding(
-              padding: Styles().bottomButtonEdgeInset,
-              child: GradientButton(
-                onPressed: () => Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const FindPage(),
-                  ),
-                ),
-                child: Text(
-                  'Begin',
-                  style: Styles().mediumTextStyle.copyWith(color: Colors.white),
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
