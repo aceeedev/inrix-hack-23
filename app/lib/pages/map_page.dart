dd00a14284f603cf49fbb5b40d81779e3a7d40e3
Leo Chen <Leo_Chen_zy@hotmail.com>
2023-11-11 23:54:37 -0800

    Co-authored-by: Isaac <iorais@users.noreply.github.com> Co-authored-by: Andrew Collins <aceeedev@users.noreply.github.com>

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -25,12 +25,6 @@ class _MapPageState extends State<MapPage> {
     mapController = controller;
   }
 
-  Polyline driveline = const Polyline(
-    polylineId: PolylineId("driveline"),
-    color: Colors.blue,
-    points: [LatLng(37.7775, -122.416389)], // replace with temp points
-    endCap: Cap.roundCap,
-  );
   // Polyline transitline =
   //     const Polyline(polylineId: PolylineId("awqeF|h_jVv@dATXCDMN"));
   // Polyline returnline = const Polyline(polylineId: PolylineId("returnline"));
@@ -44,7 +38,7 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
+      body: FutureBuilder(
         future: getRoutes(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
@@ -52,58 +46,81 @@ class _MapPageState extends State<MapPage> {
               return Center(
                   child: Text('An error has occurred, ${snapshot.error}'));
             } else if (snapshot.hasData) {
-              List<NavRoute> route = snapshot.data!;
+              List<NavRoute> routes = snapshot.data!;
+
+              List<MaterialColor> lineColors = [
+                Colors.blue,
+                Colors.orange,
+                Colors.red
+              ];
+
+              Set<Polyline> polyset = {};
+              for (int loop = 0; loop <= 0; loop++) {
+                polyset.add(Polyline(
+                    polylineId: PolylineId(routes[loop].name),
+                    points: routes[loop].latLongPairs,
+                    color: lineColors[loop]));
+              }
+
+              // example
+              // Polyline driveline = const Polyline(
+              //   polylineId: PolylineId("driveline"),
+              //   color: Colors.blue,
+              //   points: [LatLng(37.7775, -122.416389)], // replace with temp points
+              //   endCap: Cap.roundCap,
+              // );
 
               return SlidingUpPanel(
-      panel: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Route',
-              style: Styles().largeTextStyle,
-            ),
-          ),
-        ],
-      ),
-      borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
-      body: Stack(
-        children: [
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            cloudMapId: '40bae229feee19e5', // javascript vector
-            // cloudMapId: '5b05b9f927bfcf34',  // javascript raster
-            // cloudMapId: '701a336f83a1aaa6', // static raster
-            // cloudMapId: '74194f22342551fa',  // android
-            polylines: <Polyline>{driveline},
-            markers: parkSpots,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
-            myLocationButtonEnabled: false,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 32, left: 16),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                style: const ButtonStyle(splashFactory: NoSplash.splashFactory),
-                child: const Icon(Icons.arrow_back_ios_new),
-              ),
-            ),
-          ),
-        ],
-      ),
-    )
+                panel: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Route',
+                        style: Styles().largeTextStyle,
+                      ),
+                    ),
+                  ],
+                ),
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0)),
+                body: Stack(
+                  children: [
+                    GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      cloudMapId: '40bae229feee19e5', // javascript vector
+                      // cloudMapId: '5b05b9f927bfcf34',  // javascript raster
+                      // cloudMapId: '701a336f83a1aaa6', // static raster
+                      // cloudMapId: '74194f22342551fa',  // android
+                      polylines: polyset,
+                      markers: parkSpots,
+                      initialCameraPosition: CameraPosition(
+                        target: _center,
+                        zoom: 11.0,
+                      ),
+                      myLocationButtonEnabled: false,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 32, left: 16),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          style: const ButtonStyle(
+                              splashFactory: NoSplash.splashFactory),
+                          child: const Icon(Icons.arrow_back_ios_new),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
             }
           }
-
           return const Center(child: CircularProgressIndicator());
         },
       ),
-      );
+    );
   }
 }
