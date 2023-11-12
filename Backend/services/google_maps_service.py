f44ea8046299f615bbfd59a3ede3fee44fe4eb07
Riley Wong <rnwong@scu.edu>
2023-11-11 23:20:14 -0800

    Finalize google maps route

--- Backend/services/google_maps_service.py
+++ Backend/services/google_maps_service.py
@@ -1,6 +1,6 @@
 import requests
-from pprint import pprint
 import json
+from pprint import pprint
 import polyline
 
 from credentials_service import CredentialsService
@@ -23,42 +23,27 @@ class GoogleMapsService:
 
         start_info = response["routes"][0]["legs"][0]["start_address"]
         end_info = response["routes"][0]["legs"][0]["end_address"]
+        time_text = response["routes"][0]["legs"][0]["duration"]["text"]
+        time = response["routes"][0]["legs"][0]["duration"]["value"]
         try:
-            fare = response["routes"]["fare"]["text"]
+            fare_text = response["routes"][0]["fare"]["text"]
+            fare = response["routes"][0]["fare"]["value"]
         except:
+            fare_text = None
             fare = None
 
-
-        points = []
-
+        steps = []
         for step in response["routes"][0]["legs"][0]["steps"]:
 
             poly = polyline.decode(step["polyline"]["points"])
-            points.extend(poly)
+            poly = [[x, y] for x, y in poly]
 
-            # try:
-            #     sub_points = []
-            #     for sub_step in step["steps"]:
-            #         lat = sub_step["start_location"]["lat"]
-            #         long = sub_step["start_location"]["lng"]
-            #         sub_points.append([lat, long])
-            #     points.append(sub_points)
+            mode = step["travel_mode"]
 
-            # except:
-            #     lat = step["start_location"]["lat"]
-            #     long = step["start_location"]["lng"]
-            #     points.append([[lat, long]])
+            steps.append({"mode": mode, "path": poly})
 
-        pprint(points)
+        res = {"start": start_info, "end": end_info, "time_text": time_text, "time": time, "fare_text": fare_text, "fare": fare, "steps": steps}
+        # pprint(res)
+        return res
 
-        return {"start": start_info, "end": end_info, "fare": fare, "points": points}
-
-        print(points)
-
-        with open("transit.json", "w") as outfile:
-            json.dump(response, outfile)
-
-        pprint(response)
-        
-
-GoogleMapsService.run_get_transit_route(37.7786, -122.3893, 37.8024, -122.4058)
+# GoogleMapsService.run_get_transit_route(37.7786, -122.3893, 37.8024, -122.4058)
