5f5478cc3c07c0d77ad105fbad64971d8e136b08
Lucas Jablon <lucas.jablon@gmail.com>
2023-11-11 20:09:45 -0800

    Implemented Google Maps and TicketMaster Credentials in the Manager

--- Backend/services/credentials_service.py
+++ Backend/services/credentials_service.py
@@ -1,13 +1,17 @@
+from dotenv import dotenv_values
 from datetime import datetime as dt
 import requests
 
 # Call get_token for a bearer token. Can get any attribute directly such as APP_ID and APP_KEY
 class CredentialsService: 
     def __init__(self) -> None:
+        credentials = dotenv_values(".env")
         self.APP_ID = "2lsj0ylb5d"
         self.APP_KEY = "hZkt5ZDvL5aivF95xUJhU9aMjMyjmvUU98UipAJq"
         self.HASH_TOKEN = "MmxzajB5bGI1ZHxoWmt0NVpEdkw1YWl2Rjk1eFVKaFU5YU1qTXlqbXZVVTk4VWlwQUpx"
         self.format = "%Y-%m-%dT%H:%M:%S"
+        self.GOOGLE_API_KEY = credentials["GOOGLE_API_KEY"]
+        self.TICKET_MASTER_API_KEY = credentials["TICKET_MASTER_API_KEY"]
         self._get_new_token()
         
     def _get_new_token(self) -> None: 
@@ -18,7 +22,7 @@ class CredentialsService:
         }
         
         req = requests.get(url=url, params=query_params).json()
-        self.bearer_token = req["result"]["token"]
+        self.inrix_bearer_token = req["result"]["token"]
         self.expiration_date = self._parse_inrix_expiration_date(req["result"]["expiry"])
     
     def _parse_inrix_expiration_date(self, timestamp: str) -> dt:
@@ -28,8 +32,15 @@ class CredentialsService:
         '''Validates a bearer token. If the token is expired a new one is queried'''
         if not self._valid_token():
             self.get_new_token()
-        return self.bearer_token
+        return self.inrix_bearer_token
+    
+    def get_google_api_key(self) -> str:
+        return self.GOOGLE_API_KEY
+    
+    def get_ticket_master_api_key(self) -> str:
+        return self.TICKET_MASTER_API_KEY
     
     def _valid_token(self) -> bool:
         now = dt.utcnow()
         return now < self.expiration_date
+
